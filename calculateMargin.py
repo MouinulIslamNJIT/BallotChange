bd1778e3e08a5c009ff98ab25bbaaf364d52388e
DongWei0437 <dow12@pitt.edu>
2022-03-01 13:57:31 -0500

    +++++++++++++++++need to be updated

--- calculateMargin.py
+++ calculateMargin.py
@@ -5,8 +5,13 @@ import gurobipy as gp
 from gurobipy import GRB
 import math
 from multiAttributesBallotChange import *
+import random
+import networkx as nx
 
 
+
+random.seed(0)
+np.random.seed(0)
 def findq(Lv,Lc,a,b):
     cnta = 0
     cntb = 0
@@ -53,10 +58,7 @@ def calculateMargin(Lv,Lc,a,b):
     B2, U, D = findBallotChange(Lv,Lc,qmin,a,b,qmin,qmax)
     B, Umax, Dmin = findBallotChange(Lv,Lc,qmax-1,a,b,qmin,qmax)
     B, Umin, Dmax = findBallotChange(Lv,Lc,qmin+1,a,b,qmin,qmax)
-    # print(Umax,Umin)
-    # print(Dmax,Dmin)
     B3 = 0
-    # print(Dmin,Umin)
     q = -1
     if Dmin > Umax:
         B3 = Dmin
@@ -107,13 +109,11 @@ def calculateMargin_multigroup(Lv,Lc,portions):
     # print(qmin,qmax)
     # print(Umin,Umax)
     # print(Dmin, Dmax)
-    B3 = 1000000
+    B3 = 999999999
     q = -1
     if Dmin > Umax:
-        print('Dmin')
         B3 = Dmin
     elif Dmin < Umin and Dmax < Umin:
-        print('Umin')
         B3 = Umin
     else:
         qright = qmax - 1
@@ -162,7 +162,7 @@ def calculateMargin_selectTopK(Lv,Lc, K, portion):
 
 
 def AlgOptMFMultiMore(Lv, Lc, k, portions):
-    margin = 10000000
+    margin = 999999999
     Opt_t = -1
     for t in range(min(Lv), max(Lv) + 1):
         b = findBallotChangeMultiMore(Lv, Lc, k, portions, t)
@@ -181,15 +181,91 @@ def findMarginLexi(Lv, Lc, LeximinTopk,k):
             Lc[i] = 0
         else:
             Lc[i] = 1
-
     #because we want all from leximin output in top-k.
     a = [k,0]
-
     #iterate all q using findBallotChange
-    margin = 10000000
+    margin = 999999999
     for q in range(min(Lv),max(Lv)+1):
         Bq, Uq, Dq = FindBallotChangeMulti(Lv, Lc, a, q)
         if margin > Bq:
             margin = Bq
-    print(margin)
+    # print(margin)
     return margin
+
+
+def findAplusR(Lv, Lc, a, t, k):
+    A = [set(), set()]
+    for i, j in Lc:
+        A[0].add(i)
+        A[1].add(j)
+    A = [list(A[0]), list(A[1])]
+    # print(A)
+
+    # Find weights:
+    w = []
+    for i in range(len(Lv)):
+        if Lv[i] < t:
+            w.append(t - Lv[i])
+        else:
+            w.append((t - 1) - Lv[i])
+    # print(w)
+
+    # initialize graph
+    G = nx.DiGraph()
+
+    # Add source and destination
+    G.add_node("Source", demand=-k)
+    G.add_node("Destination", demand=k)
+
+    for i in range(len(A[0])):
+        # print(A[0][i],a[0][A[0][i]])
+        G.add_edge("Source", A[0][i], capacity=a[0][A[0][i]])
+    for i in range(len(A[1])):
+        # print(A[1][i],a[1][A[1][i]])
+        G.add_edge(A[1][i], "Destination", capacity=a[1][A[1][i]])
+
+    # Add edges
+    dummyWeight = {}
+    for c in range(len(Lc)):
+        i, j = Lc[c]
+        r = random.random()
+        dummy = j + "_" + str(r)
+        # dummyWeight[dummy] = w[c]
+        G.add_edge(i, dummy, weight=w[c], capacity=1)
+        G.add_edge(dummy, j, weight=0, capacity=1)
+
+    # solve min cost flow
+    flowDict = nx.min_cost_flow(G)
+    cost = nx.cost_of_flow(G, flowDict)
+
+    costAR = cost
+    for wc in w:
+        if wc < 0:
+            costAR = costAR - wc
+    # print(cost)
+    # print(costAR)
+    # print(flowDict)
+    return costAR
+
+def AlgMFMulti2(Lv,Lc,a,k):
+    OPT_ar = 999999999
+    OPT_t = -1
+    Lvunq = list(set(Lv))
+    Lvunq.reverse()
+    #print(Lvunq)
+    for i in range(len(Lvunq)):
+        #print(Lv[i],Lv[i] + 1,Lv[i+1] - 1)
+        cost1 = findAplusR(Lv,Lc,a,Lvunq[i],k)
+        cost2 = findAplusR(Lv,Lc,a,Lvunq[i]+1,k)
+        #print(i)
+        cost3 = 999999999
+        ind3 = -1
+        if i < len(Lvunq) - 1:
+            cost3 = findAplusR(Lv,Lc,a,Lvunq[i+1]-1,k)
+            ind3 = Lvunq[i+1]
+        cost,t = min((cost1,Lvunq[i]),(cost2,Lvunq[i]+1),(cost3,ind3 - 1))
+        #print(cost,t)
+        if   cost < OPT_ar:
+            OPT_ar = cost
+            OPT_t = t
+    print (OPT_ar,OPT_t)
